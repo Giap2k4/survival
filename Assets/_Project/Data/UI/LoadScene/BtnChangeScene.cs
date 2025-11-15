10995e47822baf3b7693c8512547c1052c49b778
caigiap2k4 <giap15062004@gmail.com>
2025-11-15 17:32:48 +0700

    update load scene

---
+++ Assets/_Project/Data/UI/LoadScene/BtnChangeScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    protected Button btn;
+
+    [SerializeField]
+    protected EnumBase.Scenes scene;
+
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    protected void OnClick()
+    {
+        Debug.Log(scene.ToString());
+        SceneController.instance.ChangeScene(scene);
+    }
+}
