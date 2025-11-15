16e46dc6a3f78649dfcaedafb48e56e0631b817d
caigiap2k4 <giap15062004@gmail.com>
2025-11-15 17:10:07 +0700

    Update UIManager.cs

--- Assets/_Project/Data/UI/Manager/UIManager.cs
+++ Assets/_Project/Data/UI/Manager/UIManager.cs
@@ -29,6 +29,7 @@ public class UIManager : Singleton<UIManager>
     /// <summary>
     /// Tính năng chính theo từng scene
     /// </summary>
+    [SerializeField]
     public List<MainFeatureScene> mainFeatureScene = new List<MainFeatureScene>();
 
     protected int startLayer;
