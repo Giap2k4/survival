9d63f9eb7f92de2ba4ead3faafd1d1cf27cbf231
caigiap2k4 <giap15062004@gmail.com>
2025-11-18 16:41:40 +0700

    update feature hero

--- Assets/_Project/Data/Core/DataPlayer/PlayerDataManager.cs
+++ Assets/_Project/Data/Core/DataPlayer/PlayerDataManager.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class PlayerDataManager
 {
-    
+    private static HeroManager _hero;
+
+    public static HeroManager Hero 
+    {
+        get
+        {
+            if (_hero == null) _hero = DataPlayerBase.GetModule<HeroManager>();
+            return _hero;
+        }
+
+        set => _hero = value;
+    }
 }
---
+++ Assets/_Project/Feature/UI/Hero/Script/Data/HeroModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class HeroModel
+{
+    public int id;
+    public string nameHero;
+    public int level;
+}
