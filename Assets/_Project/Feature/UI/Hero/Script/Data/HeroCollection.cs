3d193cc70267caf1a4cdc9641261fd10c6082268
caigiap2k4 <giap15062004@gmail.com>
2025-11-19 11:23:48 +0700

    add csv hero

--- Assets/_Project/Feature/UI/Hero/Script/Data/HeroCollection.cs
+++ Assets/_Project/Feature/UI/Hero/Script/Data/HeroCollection.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class HeroCollection : MonoBehaviour
+[Serializable]
+public class HeroCollection : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public HeroModel[] dataGroups;
 }
