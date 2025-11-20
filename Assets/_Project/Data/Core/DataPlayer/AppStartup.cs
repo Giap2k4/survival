4a09724c69c726d0df466b84a6b800efff1a8f40
caigiap2k4 <giap15062004@gmail.com>
2025-11-20 08:30:05 +0700

    add script

---
+++ Assets/_Project/Data/Core/DataPlayer/AppStartup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppStartup : Singleton<AppStartup>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DataPlayerBase.Init();
+    }
+}
