1c1ddeb3eb6dcae6ebb78262b9e3e639a4f3109f
caigiap2k4 <giap15062004@gmail.com>
2025-11-26 15:52:10 +0700

    update

--- Assets/_Project/Feature/Gameplay/Battle/BattleLastSurvivalMode.cs
+++ Assets/_Project/Feature/Gameplay/Battle/BattleLastSurvivalMode.cs
@@ -6,12 +6,16 @@ public class BattleLastSurvivalMode : BattleModeBase
 {
     protected override void InitData()
     {
+        InitHero();
+    }
+
+    protected void InitHero()
+    {
         int idHero = PlayerDataManager.Hero.database.idHeroSelected;
         var prefab = Resources.Load<GameObject>("Hero_" + idHero);
         GameObject obj = Instantiate(prefab);
         obj.transform.position = Vector3.zero;
 
-        Debug.Log(joystick);
         obj.GetComponent<CharacterMovement>().joystick = joystick;
         cam.Follow = obj.transform;
     }
